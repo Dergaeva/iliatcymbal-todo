d154399f2052e52290ae2e3bb2473a6395a4bfe4
iliatcymbal <iliatcymbal@gmail.com>
2017-11-25 02:18:47 +0200

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -11,22 +11,12 @@ const plugins = [
         template: 'index.html'
     }),
     new webpack.optimize.CommonsChunkPlugin({ name: 'vendor' }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new textPlugin({
+        filename: 'main.css',
+        allChunks: true
+    })
 ];
-
-if (args.env && args.env.style) {
-    plugins.push(
-        new textPlugin({
-            filename: 'main-[contenthash].css',
-            allChunks: true
-        })
-    );
-
-    styleLoader = textPlugin.extract({
-        fallback: "style-loader",
-        use: ["css-loader", "sass-loader"]
-    });
-}
 
 module.exports = {
     entry: {
@@ -46,13 +36,19 @@ module.exports = {
                 exclude: path.resolve(__dirname, 'node_modules'),
                 use: {
                     loader: 'babel-loader',
-                    options: { presets: ['env', 'react'] }
+                    options: {
+                        presets: ['env', 'react'],
+                        plugins: ['transform-class-properties']
+                    }
                 }
             },
 
             {
                 test: /\.s?css$/,
-                use: styleLoader
+                use: textPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "sass-loader"]
+                })
 
             }
         ],
@@ -62,10 +58,9 @@ module.exports = {
 
     devtool: 'source-map',
 
-    /*devServer: {
+    devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        port: 9000,
-        hot: !(args.env && args.env.style)
-    }*/
+        port: 9000
+    }
 };
